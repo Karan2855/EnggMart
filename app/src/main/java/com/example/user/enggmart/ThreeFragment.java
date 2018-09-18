a5d24aa7e5c059c0d350218ff9ddb434cdf37d9f
Ujjawal Kumar <37295517+ujjawalkumar1610@users.noreply.github.com>
2018-09-18 20:49:01 +0545

    ujjawal corrected

--- app/src/main/java/com/example/user/enggmart/ThreeFragment.java
+++ app/src/main/java/com/example/user/enggmart/ThreeFragment.java
@@ -4,17 +4,38 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 
 public class ThreeFragment extends Fragment {
 
+    ArrayList personNames = new ArrayList<>(Arrays.asList("Person 1", "Person 2", "Person 3", "Person 4", "Person 5", "Person 6", "Person 7","Person 8", "Person 9", "Person 10", "Person 11", "Person 12", "Person 13", "Person 14"));
+    ArrayList personImages = new ArrayList<>(Arrays.asList(R.drawable.bookb, R.drawable.bookc, R.drawable.bookd, R.drawable.booke, R.drawable.bookf, R.drawable.bookg, R.drawable.bookb,R.drawable.bookc, R.drawable.bookd, R.drawable.booke, R.drawable.bookf, R.drawable.bookd, R.drawable.bookg, R.drawable.bookc));
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three, container, false);
+        // get the reference of RecyclerView
+        View rootView = inflater.inflate(R.layout.fragment_three, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+        // set a GridLayoutManager with 3 number of columns , horizontal gravity and false value for reverseLayout to show the items from start to end
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),2, LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(gridLayoutManager); // set LayoutManager to RecyclerView
+        //  call the constructor of CustomAdapter to send the reference and data to Adapter
+        CustomAdapter customAdapter = new CustomAdapter(getActivity(), personNames,personImages);
+        recyclerView.setAdapter(customAdapter); // set the Adapter to RecyclerView
+        return rootView;
     }
+
 }
+
