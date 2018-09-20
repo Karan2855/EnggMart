a2f47063fd8809d89b9fd3bd471de619de10ef33
ArjitTiwary <40159683+ArjitTiwary@users.noreply.github.com>
2018-09-20 15:41:42 -0700

    all file correct by arjit

--- app/src/main/java/com/example/user/enggmart/MainActivity.java
+++ app/src/main/java/com/example/user/enggmart/MainActivity.java
@@ -102,8 +102,7 @@ public class MainActivity extends AppCompatActivity implements /*NavigationView.
             @Override
             public void onClick(View view) {
 
-                Intent i=new Intent(MainActivity.this,WritePost.class);
-                startActivity(i);
+
 
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
