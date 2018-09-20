acf3e45d7ab50b2268d8ea84e707e4c4df40bb96
Ujjawal Kumar <37295517+ujjawalkumar1610@users.noreply.github.com>
2018-09-21 04:24:17 +0545

    ujjawal corrected

---
+++ app/src/main/java/com/example/user/enggmart/WritePost.java
@@ -0,0 +1,19 @@
+package com.example.user.enggmart;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by UJJAWAL-KUMAR on 9/21/2018.
+ */
+
+public class WritePost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.write_post);
+
+    }
+}
