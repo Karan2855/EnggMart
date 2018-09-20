583c978849b954a25d8cc553df554e508faf6193
Ujjawal Kumar <37295517+ujjawalkumar1610@users.noreply.github.com>
2018-09-21 03:29:26 +0545

    ujjawal corrected(inflate post layout) and created write_post layout.

--- app/src/main/java/com/example/user/enggmart/MainActivity.java
+++ app/src/main/java/com/example/user/enggmart/MainActivity.java
@@ -102,7 +102,8 @@ public class MainActivity extends AppCompatActivity implements /*NavigationView.
             @Override
             public void onClick(View view) {
 
-
+                Intent i=new Intent(MainActivity.this,WritePost.class);
+                startActivity(i);
 
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
