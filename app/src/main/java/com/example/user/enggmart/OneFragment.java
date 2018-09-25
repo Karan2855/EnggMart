40680d7039645a833c0f208d23d81416157f04cd
Bhuvan Bhaskar <bhuvanbhaskar0176@gmail.com>
2018-09-25 23:21:28 +0530

    bhuvan correcte all file in

--- app/src/main/java/com/example/user/enggmart/OneFragment.java
+++ app/src/main/java/com/example/user/enggmart/OneFragment.java
@@ -1,10 +1,12 @@
 package com.example.user.enggmart;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,6 +24,7 @@ public class OneFragment extends Fragment {
     private PostsItemViewAdapter mPostsItemViewAdapter;
     private List<PostModel> posts;
     private RecyclerView.LayoutManager mLayoutManager;
+    private FloatingActionButton fabAddPost;
 
     public OneFragment() {
     }
@@ -32,13 +35,19 @@ public class OneFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_one, container, false);
         mPostsListView = view.findViewById(R.id.posts_list_view);
+        fabAddPost = view.findViewById(R.id.fab_add_post);
         posts = new ArrayList<>();
         fillList();
         mLayoutManager = new LinearLayoutManager(getActivity());
         mPostsListView.setLayoutManager(mLayoutManager);
         mPostsItemViewAdapter = new PostsItemViewAdapter(getActivity(), posts);
         mPostsListView.setAdapter(mPostsItemViewAdapter);
-
+        fabAddPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), WritePost.class));
+            }
+        });
         return view;
     }
 
--- app/src/main/java/com/example/user/enggmart/WritePost.java
+++ app/src/main/java/com/example/user/enggmart/WritePost.java
@@ -3,17 +3,28 @@ package com.example.user.enggmart;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by UJJAWAL-KUMAR on 9/21/2018.
  */
 
 public class WritePost extends AppCompatActivity {
+    private CircleImageView crossImage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.write_post);
-
+        crossImage = findViewById(R.id.write_post_cross);
+        crossImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
