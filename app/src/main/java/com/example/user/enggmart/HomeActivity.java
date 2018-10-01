9c302f755f8ce07bcda66ce13841131151eeaadd
Karan2855 <karanverma2855@gmail.com>
2018-10-01 15:13:07 +0530

    karan corrected

--- app/src/main/java/com/example/user/enggmart/HomeActivity.java
+++ app/src/main/java/com/example/user/enggmart/HomeActivity.java
@@ -118,6 +118,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        setupViewPager();
     }
 
     private void onClicking() {
