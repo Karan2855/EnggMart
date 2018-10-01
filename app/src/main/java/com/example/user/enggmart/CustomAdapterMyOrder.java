cd4da0b881222c63bd68984586833a15ccc01030
Ujjawal Kumar <37295517+ujjawalkumar1610@users.noreply.github.com>
2018-10-01 11:11:39 +0545

    ujjawal corrected

---
+++ app/src/main/java/com/example/user/enggmart/CustomAdapterMyOrder.java
@@ -0,0 +1,69 @@
+package com.example.user.enggmart;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapterMyOrder extends RecyclerView.Adapter {
+    ArrayList personNames;
+    ArrayList personImages;
+    Context context;
+    public CustomAdapterMyOrder(Context context, ArrayList personNames, ArrayList personImages) {
+        this.context = context;
+        this.personNames = personNames;
+        this.personImages = personImages;
+    }
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // infalte the item Layout
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_my_order, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // set the data in items
+        holder.name.setText((Integer) personNames.get(position));
+        holder.image.setImageResource((Integer) personImages.get(position));
+        // implement setOnClickListener event on item view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open another activity on item click
+                Intent intent = new Intent(context, OrderDescription.class);
+                intent.putExtra("image", (Bundle) personImages.get(position)); // put image data in Intent
+                context.startActivity(intent); // start Intent
+            }
+        });
+    }
+    @Override
+    public int getItemCount() {
+        return personNames.size();
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        // init the item view's
+        TextView name;
+        ImageView image;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            // get the reference of item view's
+            name = (TextView) itemView.findViewById(R.id.name);
+            image = (ImageView) itemView.findViewById(R.id.image);
+        }
+    }
+}
